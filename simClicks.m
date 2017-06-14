e33a02ddd310bb15cc4e1931d8e1e2ec32ea42d0
Russell Kim <therealrussellkim@nat-oitwireless-inside-vapornet100-d-4598.Princeton.EDU>
2017-06-14 13:40:51 -0400

    New code

--- simClicks.m
+++ simClicks.m
@@ -1,9 +1,30 @@
-function[simulated_num_clicks] = simClicks(avg_num_clicks, avg_num_auctions, avg_cpc, number_of_data_Points) 
+function[simulated_num_clicks] = simClicks(data, column_No, year, month, day, day_of_week, hour) 
 
-x = [avg_cpc, 1, 1, number_of_data_Points]; 
-y = avg_num_clicks/avg_num_auctions; 
+% %( 5x4 matrix(5 data points and 4 independent variables
+% x = [avg_cpc 0 1 1; 2 6 3 6; 9 34 8 2; 86 3 4 6; 1 8 4 5];
+% 
+% % 5x2 matrix(5 data points and [number_of_clicks number_of_auctions]
+% y = [number_of_clicks number_of_auctions; 3 number_of_auctions; 23 number_of_auctions; 112 number_of_auctions; 56 number_of_auctions]; 
+%  
+% x
+% y
+% logitCoef = glmfit(x,[y],'binomial'); 
+% pWC = glmval(logitCoef, x, 'logit'); 
+% 
+% simulated_num_clicks = binornd(number_of_auctions,pWC); 
 
-pWC = glmfit(x,y,'binomial'); 
+year = []; 
+month = []; 
+day = []; 
+day_of_week = []; 
+hour = []; 
+auctions = []; 
+clicks = []; 
+conversions = []; 
 
-simulated_num_clicks = binornd(avg_num_auctions,pWC); 
+for x = 1:column_No
+    year = [year data(x,4)];
+    month = [month data(x,5)]; 
+
+
 end 
--- simulator_primitive.m
+++ simulator_primitive.m
@@ -1,17 +1,21 @@
 % The main file for simulating aspects of an auction bid based on previous
 % Read in labels 
-% labels = csvread('ParsedParam.csv',0,0,    % DH: commented out -- not working as of now 
+% labels = csvread('ParsedParam.csv',0,0,    
 % Read in data
 data = csvread('ParsedParam.csv',1,0);   % DH: this reads in all data from the csv (header is not read in)
-% DH: output.csv is created by using 
+
 
 %Read in the ad number, the csv file
-prompt = 'Enter the ad number.'; 
-% ad_Number = input(prompt);   % commented out by DH: for debugging ease
-ad_Number = 14728045224;       % for debugging ease
+ad_Number = input('Enter the ad number.'); 
+year = input('Enter the last two digits of the year.'); 
+month = input('Enter the month number'); 
+day = input('Enter the day.'); 
+day_of_week = input('Enter the day of week as a number.'); 
+hour = input('Enter the hour of day.'); 
+
 
 %Get the height of the array so that we can iterate through them
-column_No = size(data,1); 
+column_No = size(data,1) - 20000; 
 % Keep track of the number of data points in each ad group
 number_of_data_Points = 0;  
 
@@ -21,14 +25,22 @@ cost_per_click = 0;
 number_of_clicks = 0; 
 value_of_clicks = 0; 
 
-for x = 1:column_No
-    if data(x,2) == ad_Number
+% Aggregate the sums of all the data points 
+for x = 1:(column_No)
+    if data(x,2) == ad_Number 
         number_of_data_Points = number_of_data_Points + 1;
         number_of_auctions = number_of_auctions + data(x,5);
         cost_per_click = cost_per_click + data(x, 17);
         number_of_clicks = number_of_clicks + data(x,7); 
         value_of_clicks = value_of_clicks + data(x,19);
     end 
+%     if all(data(x,2) == ad_Number ,data(x,3) == year ,data(x,4) == month ,data(x,5) == day ,data(x,6) == day_of_week ,data(x,7) == hour_of_day)
+%         number_of_data_Points = number_of_data_Points + 1;
+%         number_of_auctions = number_of_auctions + data(x,5);
+%         cost_per_click = cost_per_click + data(x, 17);
+%         number_of_clicks = number_of_clicks + data(x,7); 
+%         value_of_clicks = value_of_clicks + data(x,19);
+%     end 
 end 
 
 % Calculate the averages of the values 
@@ -41,7 +53,7 @@ avg_value_clicks = value_of_clicks/number_of_data_Points;
 % Simulate the number of auctions 
 sim_Auctions = simAuctions(avg_num_auctions)
 % Simulate the number of clicks
-sim_Clicks = simClicks(avg_num_clicks,avg_num_auctions,avg_cpc,number_of_data_Points)
+sim_Clicks = simClicks(data, column_No, year, month, day, day_of_week, hour)
 % Simulate the total cost
 sim_total_Cost = totalCost(avg_num_clicks,avg_cpc)
 % Simulate the total revenue 
