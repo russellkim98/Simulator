af16dcdb1aa00c0d3da2e43f1e84224613612efc
Russell Kim <therealrussellkim@nat-oitwireless-inside-vapornet100-c-3996.Princeton.EDU>
2017-08-09 10:55:30 -0400

    fixed bugs

--- simClicks.m
+++ simClicks.m
@@ -1,6 +1,5 @@
 function[simulatedClicks] = simClicks(theta0,theta1,bid)
     % input into logistic equation 
-    disp('Clicks')
     pWC = 1/(1 + exp(-theta0 - theta1 * bid));
     simulatedClicks = binornd(1,pWC);
 end 
